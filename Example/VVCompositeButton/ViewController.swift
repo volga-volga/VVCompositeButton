417701c9b3d664c6794c562ec4a393ee9aaaeb67
Konstantin <n1kron92@gmail.com>
2018-12-28 19:41:04 +0300

    Initial commit

---
+++ Example/VVCompositeButton/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  VVCompositeButton
+//
+//  Created by n1kron on 12/28/2018.
+//  Copyright (c) 2018 n1kron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
